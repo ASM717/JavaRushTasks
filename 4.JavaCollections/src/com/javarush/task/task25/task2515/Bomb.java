ce18496484ef5ae0875545edcfeaa28afc583151
ASM717 <stepan151@gmail.com>
2020-06-12 20:23:47 +0300

    spaceX

---
+++ 4.JavaCollections/src/com/javarush/task/task25/task2515/Bomb.java
@@ -0,0 +1,17 @@
+package com.javarush.task.task25.task2515;
+
+public class Bomb extends BaseObject {
+    public Bomb(double x, double y, double radius) {
+        super(x, y, radius);
+    }
+
+    @Override
+    public void draw() {
+        super.draw();
+    }
+
+    @Override
+    public void move() {
+        super.move();
+    }
+}
