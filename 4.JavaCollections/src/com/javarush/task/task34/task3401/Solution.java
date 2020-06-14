a128bebc960b1161f1fc4d7f65ce8fa38c4b2e8a
ASM717 <stepan151@gmail.com>
2020-06-14 18:29:03 +0300

    Recursia

---
+++ 4.JavaCollections/src/com/javarush/task/task34/task3401/Solution.java
@@ -0,0 +1,38 @@
+package com.javarush.task.task34.task3401;
+
+/* 
+Числа Фибоначчи с помощью рекурсии
+*/
+public class Solution {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        System.out.println(solution.fibonacci(9));     //34
+        System.out.println(solution.fibonacci(5));     //5
+        System.out.println(solution.fibonacci(2));     //1
+        System.out.println(solution.fibonacci(1));     //1
+    }
+
+    /*public int fibonacci(int n) {
+        //if(n<=0)
+        if (n == 0) {
+            return 0;
+        } if (n == 1) {
+            return 1;
+        }
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }*/
+
+    //через массив быстрее, для больших чисел n ---> Long
+
+    public int fibonacci(int n) {
+        int [] arr = new int[n + 1];
+        arr[0] = 0;
+        arr[1] = 1;
+
+        for (int i = 2; i <= n ; i++) {
+            arr[i] = arr[i - 1] + arr[i - 2];
+        }
+        return arr[n];
+    }
+}
