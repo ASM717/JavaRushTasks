f1268d057cd323e1667a1773949a76f176c4758e
ASM717 <stepan151@gmail.com>
2020-06-16 12:51:35 +0300

    Generic

---
+++ 4.JavaCollections/src/com/javarush/task/task35/task3501/Solution.java
@@ -0,0 +1,17 @@
+package com.javarush.task.task35.task3501;
+
+/* 
+Вызов статического метода
+*/
+public class Solution {
+    public static void main(String[] args) {
+        Number number = (Number) GenericStatic.<Number>someStaticMethod(new Integer(3));
+
+    }
+
+    public static void someStaticMethod() {
+
+    }
+
+
+}
